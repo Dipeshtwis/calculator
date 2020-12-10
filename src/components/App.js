9439904f6732622740de9b00290cef3feaa27b4a
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-10 18:55:11 +0530

    Add calculate function inside app component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 /* eslint-enable no-unused-vars */
 
 const App = () => (
       <React.Fragment>
-        <Display result ='Milestone 2 (No calculation till now)'/>
+        <Display result ='Milestone 4 (logic implemented)'/>
         <ButtonPanel />
       </React.Fragment>
 );
