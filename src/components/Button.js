5b65f6c3dd9448ace3eeda85ca2ed30f73f26c37
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-12 23:42:09 +0530

    Add each button to separate class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types';
-
-const Button = (props) => <button onClick = {props.clickHandler}>{props.name}</button>;
+/* eslint-disable no-unused-vars */
+const Button = (props) => {
+  const a = 9;
+  return (
+    <button className = {props.name} onClick = {props.clickHandler}>{props.name}</button>
+  );
+};
 
 Button.propTypes = {
   name: PropTypes.string,
