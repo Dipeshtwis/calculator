008400d8a57af0ab5cc2d4e37bed2efd9c3ee551
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-24 15:15:18 +0530

    Make test for all different name

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -6,4 +6,16 @@ describe("calculator logic", () => {
     expect(res).toHaveProperty('total');
     expect(res.total).toEqual(null);
   });
+
+  it("make number negative when name is equal to +/-", () => {
+  	const res = calculate({total: 25, next: null, operation: null}, '+/-');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual(-25);
+  });
+
+  it("do modulo with 100 when name is equal to %", () => {
+  	const res = calculate({total: 25, next: null, operation: null}, '%');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual(0.25);
+  });
 });
