8f0c28cad5b07b49beb8e837c08c8116da0da75a
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-24 14:02:06 +0530

    Add test for AC button

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,9 +1,9 @@
-describe("Timeline", () => {
-  it("passing test", () => {
-    expect(true).toBeTruthy();
-  });
+import calculate from '../logic/calculate';
 
-  it("failing test", () => {
-    expect(false).toBeFalsy();
+describe("calculator logic", () => {
+  it("return 0 if name is equal to AC", () => {
+  	const res = calculate({total: 25, next: 45, operation: '+'}, 'AC');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual(null);
   });
 });
