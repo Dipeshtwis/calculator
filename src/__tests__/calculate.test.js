2896bd12a4a4c54084c19d71e31a3b8396eada64
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-12-24 15:25:12 +0530

    Add test for different operation

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -18,4 +18,34 @@ describe("calculator logic", () => {
     expect(res).toHaveProperty('total');
     expect(res.total).toEqual(0.25);
   });
+
+  it("apply addition if operation is equal to +", () => {
+  	const res = calculate({total: 25, next: 45, operation: '+'}, '=');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual("70");
+  });
+
+  it("apply subtraction if operation is equal to -", () => {
+  	const res = calculate({total: 25, next: 45, operation: '-'}, '=');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual("-20");
+  });
+
+  it("apply multiplication if operation is equal to x", () => {
+  	const res = calculate({total: 5, next: 5, operation: 'x'}, '=');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual("25");
+  });
+
+  it("apply divison if operation is equal to ÷", () => {
+  	const res = calculate({total: 25, next: 5, operation: '÷'}, '=');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual("5");
+  });
+
+  it("return message error when second number is 0 while division", () => {
+  	const res = calculate({total: 25, next: 0, operation: '÷'}, '=');
+    expect(res).toHaveProperty('total');
+    expect(res.total).toEqual("Zero divide Error");
+  });
 });
